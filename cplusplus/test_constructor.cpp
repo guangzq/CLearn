7d22f2528ddac5d1f1e571f22618a9be7e5a112a
zhuqiguang <qiguang.zhu@foxmail.com>
2020-09-28 10:36:03 +0800

    添加析构函数

--- cplusplus/test_constructor.cpp
+++ cplusplus/test_constructor.cpp
@@ -3,7 +3,9 @@
 //
 #include <iostream>
 #include <cstring>
+
 using namespace std;
+
 class Student {
 private:
     char name[100];
@@ -11,20 +13,31 @@ private:
     double score;
 public:
     Student(char *n, int num1, double score1);
+
+    ~Student();
+
     void print();
 
 };
+
 Student::Student(char *n, int num1, double score1) {
     strcpy(name, n);
     num = num1;
     score = score1;
+    cout << name << " " << num << " " << score << " constructor" << endl;
 }
+
+Student::~Student() {
+    cout << name << " " << num << " " << score << " destructor" << endl;
+}
+
 void Student::print() {
-    cout<<name<<" "<<num<<" "<<score<<endl;
+    cout << name << " " << num << " " << score << endl;
 }
+
 int main() {
-    Student A("xiaoming", 01, 98.7);
-    A.print();
+    Student A("this is A", 01, 98.7);
+    Student B("this is B", 02, 98.6);
     return 0;
 }
 
