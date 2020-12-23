56bf8d5e52215f27be616233b268ef39072a84ca
zhuqiguang <qiguang.zhu@foxmail.com>
2020-12-23 18:16:41 +0800

    添加剑指offer 03题

---
+++ offer/offer_03.cpp
@@ -0,0 +1,34 @@
+//
+// Created by qiguang.zhu on 2020/12/23.
+//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    static int findRepeatNumber(vector<int> &nums) {
+        int tmp;
+        for (int i = 0; i < nums.size(); ++i) {
+            while (nums[i] != i) {
+                if (nums[i] == nums[nums[i]]) {
+                    return nums[i];
+                }
+                tmp = nums[i];
+                nums[tmp] = nums[i];
+                nums[i] = tmp;
+            }
+        }
+        return -1;
+    }
+};
+
+int main() {
+    vector<int> nums = {4, 3, 1, 0, 2, 5, 3};
+    int i = Solution::findRepeatNumber(nums);
+    cout << "count " << i << endl;
+    return 0;
+}
+
+
